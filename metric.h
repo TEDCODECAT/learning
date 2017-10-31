257c043bdab4ac34dfcee604207e430a72acd4b5
Jim Svoboda <33245155+TEDCODECAT@users.noreply.github.com>
2017-10-31 18:18:21 -0400

    Add files via upload

---
+++ metric.h
@@ -0,0 +1,11 @@
+#define IPC	0.394
+#define CPI	1 / IPC
+
+#define QPL	1.057
+#define LPQ	1 / QPL
+
+#define OPG	0.035
+#define GPO	1 / OPG
+
+#define KPM	1.609
+#define MPK	1 / KPM
---
+++ p1203.c
@@ -0,0 +1,113 @@
+/*
+   LISTEN HERE YOU LITTLE SKANKS:
+   If you dont have my particular header file titled: "metric.h"
+   you will be up shits creek without a header file. 
+
+   If you do happen to have my obscure-ass file, well congradulations.
+   There is no way this program will make sense without my acronym definitions.
+   This simple program (+the header file) allows you to convert metric to imperial measurements.
+   Honestly this is just a pet project to learn how to use macros and header files
+   you can just google this shit
+   */
+
+/* *******DEFINITION BITCHES********** */
+
+//IPC = inches per centimeter; CPI = centimeters per inch
+//QPL = quarts per liter; LPQ = liters per quart
+//OPG = ounces per gram; GPO = grams per ounces
+//MPK = miles per kilometers; KPM = kilometers per miles
+
+#include <stdio.h>
+#include "metric.h"
+
+int main (void)
+{
+	printf ("What kind of conversion would you like to perform?\n"
+"Enter one of the following please:\n"
+"For grams to ounces enter '1'\n"
+"For ounces to grams enter '2'\n"
+"For liters to quarts enter '3'\n"
+"For quarts to liters enter '4'\n"
+"For centimeters to inches enter '5'\n"
+"For inches to centimeters enter '6'\n"
+"For kilometers to miles enter '7'\n"
+"For miles to kilometers enter '8'\n"
+"If you'd like a different conversion you can exit the program now by pressing ctrl+c");
+	
+	float liters, quarts, centimeters,
+	      inches, grams, ounces, kilometers, miles, result;
+	char i;
+	printf ("\n\nEnter your choice here: ");
+	scanf ("%c", &i);
+
+	switch (i)
+	{
+		case '1':
+			printf (
+"\n\nHow many grams: ");
+			scanf ("%f", &grams);
+			result = grams * OPG;
+			printf (
+"\n%g grams is %g ounces!\n\n", grams, result);
+			break;
+		case '2':
+			printf (
+"\n\nHow many ounces: ");
+			scanf ("%f", &ounces);
+			result = ounces * GPO;
+			printf (
+"\n%g ounces is %g grams!\n\n", ounces, result);
+			break;
+		case '3':
+			printf (
+"\n\nHow many liters: ");
+			scanf ("%f", &liters);
+			result = liters * QPL;
+			printf (
+"\n%g liters is %g quarts!\n\n", liters, result);
+			break;
+		case '4':
+			printf (
+"\n\nHow many quarts: ");
+			scanf ("%f", &quarts);
+			result = quarts * LPQ;
+			printf (
+"\n%g quarts is %g liters!\n\n", quarts, result);
+			break;
+		case '5':
+			printf (
+"\n\nHow many centimeters: ");
+			scanf ("%f", &centimeters);
+			result = centimeters * IPC;
+			printf (
+"\n%g centimeters is %g inches!\n\n", centimeters, result);
+			break;
+		case '6':
+			printf (
+"\n\nHow many inches: ");
+			scanf ("%f", &inches);
+			result = inches * CPI;
+			printf (
+"\n%g inches is %g centimeters!\n\n", inches, result);
+			break;
+		case '7':
+			printf (
+"\n\nHow many kilometers: ");
+			scanf ("%f", &kilometers);
+			result = kilometers * MPK;
+			printf (
+"\n%g kilometers is %g miles!\n\n", kilometers, result);
+			break;
+		case '8':
+			printf (
+"\n\nHow many miles: ");
+			scanf ("%f", &miles);
+			result = miles * KPM;
+			printf (
+"\n%g miles is %g kilometers!\n\n", miles, result);
+			break;
+		default:
+			break;
+	}
+	return 0;
+}
